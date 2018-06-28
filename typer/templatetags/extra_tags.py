b9ad9af0d7202b54af8f68225f34e646eeec9e5a
patbiele <patryk.bielecki@o2.pl>
2018-06-28 22:20:04 +0200

    grupy zamiast kolejek wersja z playoff

--- typer/templatetags/extra_tags.py
+++ typer/templatetags/extra_tags.py
@@ -8,3 +8,18 @@ def index(List, i):
 @register.filter(name='chr')
 def chr_(value):
     return chr(value + 64)
+
+@register.filter(name='group_playoff')
+def group_playoff_(value):
+    if value < 9:
+        return 'Grupa'+chr_(value)
+    elif value == 9:
+        return 'Runda 1/8'
+    elif value == 10:
+        return 'Ćwierćfinały'
+    elif value == 11:
+        return 'Półfinały'
+    elif value == 12:
+        return 'Finał i 3. miejsce'
+    else:
+        return value
